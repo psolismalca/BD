5c477bbc59307206d342e8938bbeded0d70db249
psolismalca <31100469+psolismalca@users.noreply.github.com>
2025-05-17 14:48:37 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function mostrarOpciones(id) {
+    var elemento = document.getElementById(id);
+    if (elemento.style.display === "none") {
+        elemento.style.display = "block";
+    } else {
+        elemento.style.display = "none";
+    }
+}
